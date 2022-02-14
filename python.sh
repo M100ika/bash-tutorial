9f393136eccf1a564965c4e198e8d5b580abf72a
M100ika <maxat.suieubayev@gmail.com>
2022-02-14 22:14:34 +0600

    14.02 22:14

--- python.sh
+++ python.sh
@@ -7,3 +7,24 @@ sudo apt -y upgrade
 echo `python3 -V`
 sudo apt install -y python3-pip
 echo `python3 -V`
+
+echo ' ########################################################################################### '
+sudo apt install -y build-essential libssl-dev libffi-dev python3-dev
+
+sudo apt install -y python3-venv
+
+cd ~/.my_script_bash/git/bash-tutorial
+
+mkdir myapp && cd myapp
+echo '###########################'
+echo '###########################'
+echo '###########################'
+echo '###########################'
+echo '###########################'
+echo '###########################'
+echo '###########################'
+echo '###########################'
+
+python3 -m venv env
+source env/bin/activate
+python
